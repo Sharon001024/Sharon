7e778e3d3e4e35887e0270dad6e20d3f36952a63
謝佾諠 <124896670+Sharon001024@users.noreply.github.com>
2025-05-27 12:49:34 +0800

    Delete D1276723/D1276723 directory

--- D1276723/D1276723/js/all.js
+++
@@ -1,9 +0,0 @@
-    myFunction=()=>{
-    let txt;
-    if (confirm("已加入購物車")) {
-        txt = "You pressed OK!";
-    } else {
-        txt = "You pressed Cancel!";
-    }
-    document.getElementsByClassName("demo").innerHTML = txt;
-}
